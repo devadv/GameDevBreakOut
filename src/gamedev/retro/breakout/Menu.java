ac956d87a5f5a25601bc71f49be8f642ae1f06ca
Elcio10 <donelli@live.nl>
2015-10-07 15:05:42 +0200

    Making menu and MenuItem position of start and hightscore

--- src/gamedev/retro/breakout/Menu.java
+++ src/gamedev/retro/breakout/Menu.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.state.BasicGameState;
@@ -16,24 +17,37 @@ public class Menu extends BasicGameState {
 
 	private TrueTypeFont ttf;
 	private Font awtFont;
+	private Image image;
+	private Image image1;
 
 	public Menu(int i) {
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public void init(GameContainer container, StateBasedGame game){
-	// load a default java font
+	public void init(GameContainer container, StateBasedGame game) {
+		// load a default java font
 		Font awtFont = new Font("emulogic", Font.BOLD, 24);
 		ttf = new TrueTypeFont(awtFont, false);
+
+		try {
+			image = new Image("res/menu.png");
+			image1 = new Image("res/menuItem.png");
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
-	
 
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g)
 			throws SlickException {
-		ttf.drawString(300,400, "Mario Break Out V1");
-		
+		image.draw(0,0,800,600);
+		image1.draw(280,385);
+		ttf.drawString(320, 375, "Start");
+		ttf.drawString(320, 455, "Hightscore");
+
 	}
 
 	@Override
